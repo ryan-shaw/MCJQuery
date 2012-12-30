40be3655d70362929686193b3a7b14c5bd0242f0
yawkat <me@yawk.at>
2012-12-30 19:55:17 +0100

    Recoded Query system

--- vc/min/ryanshaw/Query/Query.java
+++ vc/min/ryanshaw/Query/Query.java
@@ -3,175 +3,235 @@ package vc.min.ryanshaw.Query;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Query extends Thread{
-	private static String host;
-	private static int port;
-	private static HashMap<String, String> values = new HashMap<String, String>();
-	private String playersString = "";
-	public int status = -1;
-	
-	public Query(String host, int port){
-		this.host = host;	//Ignore how these are accessed.
-		this.port = port;
-	}
-	
-	public boolean startQuery(){
-		
-		try {
-			status = 1;
-			Socket socket = new Socket();
-			socket.connect(new InetSocketAddress(host, port), 1000);
-		} catch (Exception e){
-			status = 2;
-		}
-		
-		
-		if(status == 1){
-			this.start();
-			return true;
-			
-		}
-		else
-			return false;
-	}
-	
-	/*public static void main(String[] args){
-		host = "minecraft.clanrks.com";
-		port = 25565;
-		new Query().start();
-	}*/
-	
-	public String getValue(String key){
-		if(values.containsKey(key))
-			return values.get(key);
-		else
-			return "not found";
-		
-	}
-	
-	public String getPlayers(){
-		return playersString;
-		
-	}
-	
-	@Override
-	public void run(){
-		playersString = "";
-		DatagramSocket socket = null;
-		try {
-			socket = new DatagramSocket();
-		} catch (SocketException e1) {
-		}
-		try {
-			InetAddress ipAddress = InetAddress.getByName(host);
-			byte[] sendData = new byte[10240]; // These seem like inefficient memory usage :(
-			byte[] receiveData = new byte[10240];
-			sendData = new byte[]{(byte)0xFE, (byte)0xFD, 0x09, 0x01, 0x01, 0x01, 0x01}; // Create handshake packet
-			socket.setSoTimeout(2000); // Socket timeout 2000 millisecs
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
-			socket.send(sendPacket); // Send handshake
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			socket.receive(receivePacket); // Receive reply
-
-			byte[] preData = receivePacket.getData(); // Pre-processed data
-			int byte1 = -1;
-			int i = 0;
-			byte[] buffer = new byte[8]; // For challenge token
-			
-			for(int count = 5; (byte1 = preData[count++]) != 0;) // Add bytes to challenge token
-				buffer[i++] = (byte) byte1;
-			
-			int challengeInt = Integer.parseInt(new String(buffer).trim()); // Convert byte array to integer
-			
-			ByteBuffer bBuff = ByteBuffer.allocate(4); // Convert challenge int to byte array
-			bBuff.putInt(challengeInt);
-			
-			sendData = concat(new byte[]{(byte)0xFE, (byte)0xFD, 0x00, 0x01, 0x01, 0x01, 0x01},bBuff.array()); // Magic bytes with session ID appended with challenge token
-			sendData = concat(sendData, new byte[]{0x00, 0x00, 0x00, 0x00}); // Append padding
-			sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port); // Create send packet
-			socket.send(sendPacket); // Send packet
-			receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			socket.receive(receivePacket); // Receive packet
-			
-			boolean onKey = true; // Are we on the key or the value			
-			
-			ArrayList<Byte> key = new ArrayList<Byte>(); // Builds up the key bytes in each cell
-			ArrayList<Byte> value = new ArrayList<Byte>(); // Builds up the value bytes in each cell
-			int i1;
-			for(i1 = 5; i1 < receivePacket.getLength(); i1++){
-				byte b = receivePacket.getData()[i1];
-				if(b == 0){
-					if(!onKey){ // If byte is 0 and not on key build the K/V and add the K/V to hashmap
-						byte[] bufferKey = new byte[key.size()];
-						if(key.size() == 1)
-							break;
-						for(i = 0; i < key.size(); i++){
-							bufferKey[i] = key.get(i);
-						}
-						byte[] bufferValue = new byte[value.size()];
-						
-						for(i = 0; i < value.size(); i++){
-							bufferValue[i] = value.get(i);
-						}
-						values.put(new String(bufferKey).trim(), new String(bufferValue).trim());
-						key.clear();
-						value.clear();
-					}
-					onKey = !onKey;
-				}
-				if(!onKey) //Add byte to appropiate list
-					value.add(b);
-				else 
-					key.add(b);
-			}
-			//for(String key1 : values.keySet()) // Debug values
-			//	System.out.println(key1 + " : "+ values.get(key1));
-			
-			byte[] buffer1 = new byte[32]; // Bytes should be enough for minecraft usernames...
-			int count2 = 0;
-			ArrayList<String> players = new ArrayList<String>();
-			
-			for(int c1 = i1; c1 < receivePacket.getLength(); c1++){
-				byte b = receivePacket.getData()[c1];
-				if(b == 0){
-					players.add(new String(buffer1).trim());
-					players.remove((Object)""); // Increase blank names got added
-					buffer1 = new byte[32];
-					count2 = 0;
-					continue;
-				}else
-				buffer1[count2++] = b;
-			}
-			 
-			for(i = 0; i < players.size(); i++){ // Build player string
-				playersString = new StringBuilder(playersString).append(players.get(i)).append(",").toString();
-			}
-			if(playersString.length() != 0)
-				playersString = playersString.substring(0,playersString.length()-1); // Take away comma on end
-		} catch (SocketTimeoutException e) {
-			status = 2;
-		} catch (IOException e) {
-			status = 2;
-		}
-		status = 3;
-	}
-	/*
-	 * Concatenate two byte arrays together
+/**
+ * Send a query to a given minecraft server and store any metadata and the
+ * player list.
+ * 
+ * @author Ryan Shaw, Jonas Konrad
+ */
+public class Query {
+	/**
+	 * The target address and port
 	 */
-	private static byte[] concat(byte[] A, byte[] B) {
-	   byte[] C= new byte[A.length+B.length];
-	   System.arraycopy(A, 0, C, 0, A.length);
-	   System.arraycopy(B, 0, C, A.length, B.length);
-
-	   return C;
+	private InetSocketAddress	address;
+	/**
+	 * <code>null</code> if no successful request has been sent, otherwise a Map
+	 * containing any metadata received except the player list
+	 */
+	private Map<String, String>	values;
+	/**
+	 * <code>null</code> if no successful request has been sent, otherwise an
+	 * array containing all online player usernames
+	 */
+	private String[]			onlineUsernames;
+	
+	/**
+	 * Convenience constructor
+	 * 
+	 * @see Query#Query(InetSocketAddress)
+	 * @param host
+	 *            The target host
+	 * @param port
+	 *            The target port
+	 */
+	public Query(String host, int port) {
+		this(new InetSocketAddress(host, port));
+	}
+	
+	/**
+	 * Create a new instance of this class
+	 * 
+	 * @param address
+	 *            The servers IP-address
+	 */
+	public Query(InetSocketAddress address) {
+		this.address = address;
+	}
+	
+	/**
+	 * Try pinging the server and then sending the query
+	 * 
+	 * @see Query#pingServer()
+	 * @see Query#sendQueryRequest()
+	 * @throws IOException
+	 *             If the server cannot be pinged
+	 */
+	public void sendQuery() throws IOException {
+		if(pingServer()) {
+			// Do the actual request
+			sendQueryRequest();
+		} else {
+			throw new IOException("Server can not be pinged");
+		}
+	}
+	
+	/**
+	 * Try pinging the server
+	 * 
+	 * @return <code>true</code> if the server can be reached within one second
+	 */
+	private boolean pingServer() {
+		// try pinging the given server
+		try {
+			final Socket socket = new Socket();
+			socket.connect(address, 1000);
+			socket.close();
+			return true;
+		} catch(IOException e) {}
+		return false;
+	}
+	
+	/**
+	 * Get the additional values if the Query has been sent
+	 * 
+	 * @return The data
+	 * @throws IllegalStateException
+	 *             if the query has not been sent yet or there has been an error
+	 */
+	public Map<String, String> getValues() {
+		if(values == null)
+			throw new IllegalStateException("Query has not been sent yet!");
+		else
+			return values;
+	}
+	
+	/**
+	 * Get the online usernames if the Query has been sent
+	 * 
+	 * @return The username array
+	 * @throws IllegalStateException
+	 *             if the query has not been sent yet or there has been an error
+	 */
+	public String[] getOnlineUsernames() {
+		if(onlineUsernames == null)
+			throw new IllegalStateException("Query has not been sent yet!");
+		else
+			return onlineUsernames;
+	}
+	
+	/**
+	 * Request the UDP query
+	 * 
+	 * @throws IOException
+	 *             if anything goes wrong during the request
+	 */
+	private void sendQueryRequest() throws IOException {
+		final DatagramSocket socket = new DatagramSocket(200);
+		try {
+			final byte[] receiveData = new byte[10240];
+			socket.setSoTimeout(2000);
+			sendPacket(socket, address, 0xFE, 0xFD, 0x09, 0x01, 0x01, 0x01, 0x01);
+			final int challengeInteger;
+			{
+				receivePacket(socket, receiveData);
+				byte byte1 = -1;
+				int i = 0;
+				byte[] buffer = new byte[8];
+				for(int count = 5; (byte1 = receiveData[count++]) != 0;)
+					buffer[i++] = byte1;
+				challengeInteger = Integer.parseInt(new String(buffer).trim());
+			}
+			sendPacket(socket, address, 0xFE, 0xFD, 0x00, 0x01, 0x01, 0x01, 0x01, challengeInteger >> 24, challengeInteger >> 16, challengeInteger >> 8, challengeInteger, 0x00, 0x00, 0x00, 0x00);
+			
+			final int length = receivePacket(socket, receiveData).getLength();
+			values = new HashMap<String, String>();
+			final AtomicInteger cursor = new AtomicInteger(5);
+			while(cursor.get() < length) {
+				final String s = readString(receiveData, cursor);
+				if(s.length() == 0)
+					break;
+				else {
+					final String v = readString(receiveData, cursor);
+					values.put(s, v);
+				}
+			}
+			readString(receiveData, cursor);
+			final Set<String> players = new HashSet<String>();
+			while(cursor.get() < length) {
+				final String name = readString(receiveData, cursor);
+				if(name.length() > 0)
+					players.add(name);
+			}
+			onlineUsernames = players.toArray(new String[players.size()]);
+		} finally {
+			socket.close();
+		}
+	}
+	
+	/**
+	 * Helper method to send a datagram packet
+	 * 
+	 * @param socket
+	 *            The connection the packet should be sent through
+	 * @param targetAddress
+	 *            The target IP
+	 * @param data
+	 *            The byte data to be sent
+	 * @throws IOException
+	 */
+	private final static void sendPacket(DatagramSocket socket, InetSocketAddress targetAddress, byte... data) throws IOException {
+		DatagramPacket sendPacket = new DatagramPacket(data, data.length, targetAddress.getAddress(), targetAddress.getPort());
+		socket.send(sendPacket);
+	}
+	
+	/**
+	 * Helper method to send a datagram packet
+	 * 
+	 * @see Query#sendPacket(DatagramSocket, InetSocketAddress, byte...)
+	 * @param socket
+	 *            The connection the packet should be sent through
+	 * @param targetAddress
+	 *            The target IP
+	 * @param data
+	 *            The byte data to be sent, will be cast to bytes
+	 * @throws IOException
+	 */
+	private final static void sendPacket(DatagramSocket socket, InetSocketAddress targetAddress, int... data) throws IOException {
+		final byte[] d = new byte[data.length];
+		int i = 0;
+		for(int j : data)
+			d[i++] = (byte)(j & 0xff);
+		sendPacket(socket, targetAddress, d);
+	}
+	
+	/**
+	 * Receive a packet from the given socket
+	 * 
+	 * @param socket
+	 *            the socket
+	 * @param buffer
+	 *            the buffer for the information to be written into
+	 * @return the entire packet
+	 * @throws IOException
+	 */
+	private final static DatagramPacket receivePacket(DatagramSocket socket, byte[] buffer) throws IOException {
+		final DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+		socket.receive(dp);
+		return dp;
+	}
+	
+	/**
+	 * Read a String until 0x00
+	 * 
+	 * @param array
+	 *            The byte array
+	 * @param cursor
+	 *            The mutable cursor (will be increased)
+	 * @return The string
+	 */
+	private final static String readString(byte[] array, AtomicInteger cursor) {
+		final int startPosition = cursor.incrementAndGet();
+		for(; cursor.get() < array.length && array[cursor.get()] != 0; cursor.incrementAndGet())
+			;
+		return new String(Arrays.copyOfRange(array, startPosition, cursor.get()));
 	}
 }
