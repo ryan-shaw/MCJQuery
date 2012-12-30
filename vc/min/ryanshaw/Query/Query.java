4dfe04883887784f8a98d6640282c3af2b623a9b
Ryan Shaw <ryans@guestsort.com>
2012-12-30 18:11:38 +0000

    Comments and some value changes

--- vc/min/ryanshaw/Query/Query.java
+++ vc/min/ryanshaw/Query/Query.java
@@ -73,41 +73,44 @@ public class Query extends Thread{
 		}
 		try {
 			InetAddress ipAddress = InetAddress.getByName(host);
-			byte[] sendData = new byte[10240];
+			byte[] sendData = new byte[10240]; // These seem like inefficient memory usage :(
 			byte[] receiveData = new byte[10240];
-			sendData = new byte[]{(byte)0xFE, (byte)0xFD, 0x09, 0x01, 0x01, 0x01, 0x01};
-			socket.setSoTimeout(2000);
+			sendData = new byte[]{(byte)0xFE, (byte)0xFD, 0x09, 0x01, 0x01, 0x01, 0x01}; // Create handshake packet
+			socket.setSoTimeout(2000); // Socket timeout 2000 millisecs
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
-			socket.send(sendPacket);
+			socket.send(sendPacket); // Send handshake
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			socket.receive(receivePacket);
-			status = 1;
-			byte[] preData = receivePacket.getData();
+			socket.receive(receivePacket); // Receive reply
+
+			byte[] preData = receivePacket.getData(); // Pre-processed data
 			int byte1 = -1;
 			int i = 0;
-			byte[] buffer = new byte[8];
-			for(int count = 5; (byte1 = preData[count++]) != 0;)
+			byte[] buffer = new byte[8]; // For challenge token
+			
+			for(int count = 5; (byte1 = preData[count++]) != 0;) // Add bytes to challenge token
 				buffer[i++] = (byte) byte1;
-			int challengeInt = Integer.parseInt(new String(buffer).trim());
-			ByteBuffer bBuff = ByteBuffer.allocate(4);
+			
+			int challengeInt = Integer.parseInt(new String(buffer).trim()); // Convert byte array to integer
+			
+			ByteBuffer bBuff = ByteBuffer.allocate(4); // Convert challenge int to byte array
 			bBuff.putInt(challengeInt);
 			
-			sendData = concat(new byte[]{(byte)0xFE, (byte)0xFD, 0x00, 0x01, 0x01, 0x01, 0x01},bBuff.array());
-			sendData = concat(sendData, new byte[]{0x00, 0x00, 0x00, 0x00});
-			sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
-			socket.send(sendPacket);
+			sendData = concat(new byte[]{(byte)0xFE, (byte)0xFD, 0x00, 0x01, 0x01, 0x01, 0x01},bBuff.array()); // Magic bytes with session ID appended with challenge token
+			sendData = concat(sendData, new byte[]{0x00, 0x00, 0x00, 0x00}); // Append padding
+			sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port); // Create send packet
+			socket.send(sendPacket); // Send packet
 			receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			socket.receive(receivePacket);
-			boolean onKey = true;
+			socket.receive(receivePacket); // Receive packet
 			
+			boolean onKey = true; // Are we on the key or the value			
 			
-			ArrayList<Byte> key = new ArrayList<Byte>();
-			ArrayList<Byte> value = new ArrayList<Byte>();
+			ArrayList<Byte> key = new ArrayList<Byte>(); // Builds up the key bytes in each cell
+			ArrayList<Byte> value = new ArrayList<Byte>(); // Builds up the value bytes in each cell
 			int i1;
 			for(i1 = 5; i1 < receivePacket.getLength(); i1++){
 				byte b = receivePacket.getData()[i1];
 				if(b == 0){
-					if(!onKey){
+					if(!onKey){ // If byte is 0 and not on key build the K/V and add the K/V to hashmap
 						byte[] bufferKey = new byte[key.size()];
 						if(key.size() == 1)
 							break;
@@ -125,7 +128,7 @@ public class Query extends Thread{
 					}
 					onKey = !onKey;
 				}
-				if(!onKey)
+				if(!onKey) //Add byte to appropiate list
 					value.add(b);
 				else 
 					key.add(b);
@@ -133,7 +136,7 @@ public class Query extends Thread{
 			//for(String key1 : values.keySet()) // Debug values
 			//	System.out.println(key1 + " : "+ values.get(key1));
 			
-			byte[] buffer1 = new byte[1024];
+			byte[] buffer1 = new byte[32]; // Bytes should be enough for minecraft usernames...
 			int count2 = 0;
 			ArrayList<String> players = new ArrayList<String>();
 			
@@ -141,19 +144,19 @@ public class Query extends Thread{
 				byte b = receivePacket.getData()[c1];
 				if(b == 0){
 					players.add(new String(buffer1).trim());
-					players.remove((Object)"");
-					buffer1 = new byte[1024];
+					players.remove((Object)""); // Increase blank names got added
+					buffer1 = new byte[32];
 					count2 = 0;
 					continue;
 				}else
 				buffer1[count2++] = b;
 			}
 			 
-			for(i = 0; i < players.size(); i++){
+			for(i = 0; i < players.size(); i++){ // Build player string
 				playersString = new StringBuilder(playersString).append(players.get(i)).append(",").toString();
 			}
 			if(playersString.length() != 0)
-				playersString = playersString.substring(0,playersString.length()-1);
+				playersString = playersString.substring(0,playersString.length()-1); // Take away comma on end
 		} catch (SocketTimeoutException e) {
 			status = 2;
 		} catch (IOException e) {
@@ -161,7 +164,9 @@ public class Query extends Thread{
 		}
 		status = 3;
 	}
-	
+	/*
+	 * Concatenate two byte arrays together
+	 */
 	private static byte[] concat(byte[] A, byte[] B) {
 	   byte[] C= new byte[A.length+B.length];
 	   System.arraycopy(A, 0, C, 0, A.length);
